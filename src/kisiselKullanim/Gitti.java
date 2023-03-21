40e9873bc4363a64b6ae57302c40648751da0c46
Yavuz Seven <yavsev97@hotmail.com>
2023-03-21 21:49:16 +0300

    ilk değişiklik

--- src/kisiselKullanim/Gitti.java
+++ src/kisiselKullanim/Gitti.java
@@ -3,5 +3,7 @@ package kisiselKullanim;
 public class Gitti {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("That's it");
+
     }
 }
